78c85e705df710c9c922ddac97eaa7162f344175
andreeachiorean24 <91500862+andreeachiorean24@users.noreply.github.com>
2021-10-13 10:46:15 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,34 +1,137 @@
-'''
-Returneaza true daca n este prim si false daca nu.
-'''
-def is_prime(n):
-  # codul vostru aici
-  
-  
-'''
-Returneaza produsul numerelor din lista lst.
-'''
-def get_product(lst):
-  # codul vostru aici
-  
-  
-'''
-Returneaza CMMDC a doua numere x si y folosind primul algoritm.
-'''
-def get_cmmdc_v1(x, y):
-  # codul vostru aici
-  
-  
-'''
-Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
-'''
-def get_cmmdc_v2(x, y):
-  # codul vostru aici
-hhhhh
-  
-  
+def citireLista():
+    list = []
+    n = int(input("Dati numarul de elem din lista: "))
+    for i in range(n):
+        list.append(int(input("list[" + str(i) + "]= ")))
+    return list
+
+
+def isPrime(nr):
+    """
+  Determina daca un numar este prim.
+  :param x: nr intreg
+  :return: True daca numarul este prim sau False in caz contrar.
+  """
+    if nr < 2:
+        return False
+    else:
+        for i in range(2,nr):
+            if nr % i == 0:
+                return False
+    return True
+
+
+def testPrimeFunction():
+    assert isPrime(7) == True
+    assert isPrime(9) == False
+    assert isPrime(2) == True
+
+
+def noneArePrimes(list):
+    """
+  Determina daca toate numerele dintr-o lista sunt prime
+  :param l: O lista de numere intregi
+  :return: True daca toate numerele dintr-o lista sunt prime sau False, in caz contar.
+  """
+    for i in list:
+        if isPrime(i):
+            return False
+    return True
+
+
+def testNonePrimesFunction():
+    assert noneArePrimes([]) == True
+    assert noneArePrimes([1, 2, 3]) == False
+
+
+def get_longest_all_not_primes(list):
+    """
+    Determina cea mai lunga subsecventa de numere neprime.
+    :param list: O lista de numere intregi
+    :return: Afiseaza cea mai lunga subsecventa de numere neprime dintr-o lista.
+    """
+    secvMax = []
+
+    for i in range(len(list)):
+        for j in range(i, len(list)):
+            if noneArePrimes(list[i: j + 1]) and len(list[i: j + 1]) > len(secvMax):
+                secvMax = list[i: j + 1]
+    return secvMax
+
+
+def testLongestNotPrimesFunction():
+    assert get_longest_all_not_primes([]) == []
+    assert get_longest_all_not_primes([1, 2, 3, 4, 6, 8, 12, 14, 5, 7, 2, 8, 10, 26]) == [4, 6, 8,  12, 14]
+    assert get_longest_all_not_primes([2, 3, 5, 8, 6, 7]) == [8, 6]
+
+
+def MediaElementelor(list):
+    """
+    Calculeaza media aritmetica a tuturor elementelor din lista
+    :param list: lista de nr. intregi
+    :return: media aritmetica a elementelor listei
+    """
+    suma = 0
+    for i in list:
+        suma = suma + i
+    media = suma/len(list)
+    return media
+
+def TestMediaElementelor():
+    assert MediaElementelor([1, 4, 4]) == 3
+    assert MediaElementelor([12]) == 12
+    assert MediaElementelor([10, 4]) == 7
+
+def get_longest_average_below(list, average):
+    """
+    Determina cea mai lunga subsecventa in care toate elementele au media mai mica decat valoarea citita
+    :param list: lista de nr. intregi
+    :param average: valoarea citita (un numar intreg) cu care comparam media
+    :return: Cea mai lunga subsecventa in care toate elementele din lista au media mai mica decat o valoare citita
+    """
+    subsecventaMax = []
+    for i in range(len(list)):
+        for j in range(i, len(list)):
+            if MediaElementelor(list[i:j + 1]) < average and len(subsecventaMax) < len(list[i:j + 1]):
+                subsecventaMax = list[i:j + 1]
+    return subsecventaMax
+
+def test_get_longest_average_below():
+    assert get_longest_average_below([2, 4, 3, 55, 23], 5) == [2, 4, 3]
+    assert get_longest_average_below([1, 2, 6, 12], 5) == [1, 2, 6]
+    assert get_longest_average_below([5, 3, 22, 23], 10) == [5,3]
+
+
 def main():
-  # interfata de tip consola aici
+
+    testPrimeFunction()
+    testNonePrimesFunction()
+    testLongestNotPrimesFunction()
+    test_get_longest_average_below()
+
+
+    list = []
+
+    while True:
+        print("1. Citire date")
+        print("2. Determinati cea mai lunga subsecventa de numere neprime dintr-o lista")
+        print("3. Determinare cea mai lungă subsecvență in care media numerelor nu depășește o valoare citită")
+        print("4. Iesire")
+
+        optiune = input("Selectati optiune: ")
+
+        if optiune == "1":
+            list = citireLista()
+        elif optiune == "2":
+            print(get_longest_all_not_primes(list))
+        elif optiune == "3":
+            average = int(input("Dati valoarea cu care sa se compare media:"))
+            print(get_longest_average_below(list, average))
+        elif optiune == "4":
+            break
+        else:
+            print("Optiune gresita! Selectati alta optiune.")
+
 
 if __name__ == '__main__':
-  main()
+    main()
